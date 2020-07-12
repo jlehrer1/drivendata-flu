c78418cb53f286fc5630d6ed0367a5bf53b93346
Julian Lehrer <jmlehrer@ucsc.edu>
2020-07-12 11:18:18 -0700

    Generated file structure for top level data/

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,12 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import urllib.request
+import os.path
 
+SUBM_FORM = "https://s3.amazonaws.com/drivendata-prod/data/66/public/submission_format.csv"
+TRAIN_FEATURES = "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_features.csv"
+TRAIN_LABELS = "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_labels.csv"
+TEST_FEATURES = "https://s3.amazonaws.com/drivendata-prod/data/66/public/test_set_features.csv"
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+print("Downloading datasets...")
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+for url in [SUBM_FORM, TRAIN_FEATURES, TRAIN_LABELS, TEST_FEATURES]:
+    urllib.request.urlretrieve(url)
